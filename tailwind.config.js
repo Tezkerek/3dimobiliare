d8e0ae027b94ab2d52ca6743091fa76cdf60b3ca
Tezkerek <andrei.ancuta@gmail.com>
2020-12-26 17:07:59 +0200

    Add contact section to homepage and change some styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,11 @@ module.exports = {
             deep: '#2a2a2b'
         },
         orange: colors.orange['600'],
-        blue: colors.blue['400'],
+        blue: {
+            light: colors.blue['500'],
+            dark: colors.blue['800']
+        },
+        green: colors.green['600'],
         purple: colors.purple['600']
     },
     extend: {},
